bb179f1ff9aa074e902fdffdb3c460431f7c7ec0
Michael Nigh <mic.nigh@gmail.com>
2017-09-03 09:54:10 -0700

    fix - bad args broke pdf build

--- scripts/buildPdf.tsx
+++ scripts/buildPdf.tsx
@@ -17,9 +17,9 @@ export const run = async () => {
   const server = await new Promise<http.Server>(resolve => {
     const newServer = app.listen(port, `0.0.0.0`, () => resolve(newServer));
   });
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   try {
-    const page = await browser.newPage({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
     await page.goto(`http://127.0.0.1:${port}/`, {
       waitUntil: 'networkidle',
       networkIdleTimeout: 5000,
