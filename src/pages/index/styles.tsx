be2b1a1be181aefcb7f5bfbfe63a113b01065a0f
Michael Nigh <mic.nigh@gmail.com>
2017-09-03 21:50:15 -0700

    move fonts to global css file for better prod performance

--- src/pages/index/styles.tsx
+++ src/pages/index/styles.tsx
@@ -1,6 +1,7 @@
 import { default as styled, injectGlobal, css } from 'styled-components';
 import 'normalize.css';
 import 'tippy.js/dist/tippy.css';
+import './index.css';
 
 export const color = {
   text: `#333`,
@@ -26,27 +27,6 @@ export const Container = styled.div`
   color: ${color.text};
 `;
 
-export const loadFontFace = (name: string) => {
-  return css`
-  @font-face {
-    font-family: '${name}';
-    src: url('${require(`../fonts/${name}.eot`)}');
-    src: url('${require(`../fonts/${name}.eot`)}?#iefix') format('embedded-opentype'),
-         url('${require(`../fonts/${name}.woff`)}') format('woff'),
-         url('${require(`../fonts/${name}.ttf`)}') format('truetype'),
-         url('${require(`../fonts/${name}.svg`)}#${name}') format('svg');
-    font-weight: normal;
-    font-style: normal;
-  }
-  `;
-};
-
 injectGlobal`
-${loadFontFace('capture-it')}
-${loadFontFace('special-elite')}
-
-.tippy-tooltip {
-  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif
-}
 
 `;
