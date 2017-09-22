d1fee6c10ffdeec20aa1900854f2e17130a02a5d
Michael Nigh <mic.nigh@gmail.com>
2017-09-22 11:03:32 -0700

    add default redux state

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,4 +1,12 @@
-const reducer = (state = {}, action: any) => {
+const reducer = (
+  state = {
+    entities: {
+      tags: {},
+      experiences: {},
+      projects: {},
+    },
+  },
+  action: any) => {
   return state;
 };
 
