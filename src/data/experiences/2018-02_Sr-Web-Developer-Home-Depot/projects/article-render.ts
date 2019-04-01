b3c1b08261a744d90a0c3da2edfb4a327f6c8273
Michael Nigh <mic.nigh@gmail.com>
2019-04-01 18:27:17 -0500

    word smithing

--- src/data/experiences/2018-02_Sr-Web-Developer-Home-Depot/projects/article-render.ts
+++ src/data/experiences/2018-02_Sr-Web-Developer-Home-Depot/projects/article-render.ts
@@ -11,17 +11,17 @@ export let end = `2019-03`;
 export let duration = moment.duration(moment().endOf(`month` as any).diff(moment(start))).toJSON();
 
 export let summaryMarkdown = `
-Work with product owners to optimize SEO and create new article content as lead front end engineer.
+Work with product owners to optimize SEO and render new article content as lead front end engineer.
 
-Analyze SEO impact of React with THD architects, especially usage as an SPA, element replacement, and SSR.
+Analyze SEO impact of React usage with THD architects as an SPA, element replacement, and SSR.
 
-Create automated tests to audit content.
+Create automated tests to validate all new and existing content is rendered correctly.
 
 Implement image snapshot differential testing for components at various states.
 
-Work with multiple teams to improve content testing.
+Work with multiple teams to improve testing process throughout deployments.
 
-Develop strategies for NextGen features.
+Develop strategies to implement NextGen THD features.
 
 `;
 
